e4ecc73b2a0c54e034717381a9492845dd9f3984
hishaam ahmad <wizhimo95@gmail.com>
2022-05-08 15:02:05 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 50px;
+
+`
+
+function App() {
+  return (
+    <Title>
+      hello world!
+    </Title>
+  );
+}
+
+export default App;
